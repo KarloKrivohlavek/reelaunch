feb03ea293d26193e06df855161419c1b39a4af3
KarloKrivohlavek <karlo.krivohlavek@hotmail.com>
2026-04-18 17:41:25 +0200

    output server addded to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import netlify from '@astrojs/netlify';
 
 
 export default defineConfig({
+  output: 'server', 
   adapter: netlify(),
   site: process.env.PUBLIC_SITE_URL  ?? 'http://localhost:4321',
 
